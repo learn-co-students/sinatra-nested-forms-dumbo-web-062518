cb969b02b92b98baded2431a45ce524ca6688946
runandrerun <the.asantiagojr@gmail.com>
2018-07-18 09:28:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,20 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @name = params["pirate"]["name"]
+      @weight = params["pirate"]["weight"]
+      @height = params["pirate"]["height"]
+      @ship_arr = params[:pirate][:ships]
+      erb :'pirates/show'
+    end
   end
 end
